05afc26162e99670fbacf8be51a710618c56fc91
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-11-20 21:38:35 +0100

    Creates fibonacci sequence

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+
+        for (int i = 2; i < 35; i++) {
+            int x = (list.get(i - 1) + list.get(i - 2));
+            if(x <=4000000) {
+                list.add(x);
+            } else {
+                break;
+            }
+        }
+            System.out.println(list);
     }
+
+
 }
